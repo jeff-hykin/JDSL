17f42bdf3dd5a0989ac3872d288dd54bdada6484
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-22 20:53:11 -0500

    '--'

--- init.js
+++ init.js
@@ -9,131 +9,136 @@ import javascript from "https://github.com/jeff-hykin/common_tree_sitter_languag
 const parser = await parserFromWasm(javascript)
 
 
-const filePaths = await FileSystem.listFileItemsIn(Deno.args[0])
-await run`git add -A`
-await run`git commit -m '--'`
-const startingCommit = (await run`git rev-parse --abbrev-ref HEAD`)
+try {
+    
+    const filePaths = await FileSystem.listFileItemsIn(Deno.args[0])
+    await run`git add -A`
+    await run`git commit -m '--'`
+    const startingCommit = (await run`git rev-parse --abbrev-ref HEAD`)
 
-const classes = {}
-FileSystem.cwd = Deno.args[0]
-for (const each of filePaths) {
-    if (each.path.endsWith(".json")) {
-        console.group()
-        console.debug(`loading ${each.path}`)
-        const parentPath = FileSystem.parentPath(each.path)
-        console.log(`        ${await run`git checkout ${startingCommit} ${Stdout(returnAsString)}`}`)
-        const output = await FileSystem.read(each.path)
-        if (!output) {
-            console.debug(`each.path: ${each.path}`)
-        }
-        try {
-            var { File, Class, Author, Purpose, Functions } = JSON.parse(output)
-        } catch (error) {
-            console.debug(`await FileSystem.listFileItemsIn(FileSystem.parentPath(each.path)) is:`,await FileSystem.listFileItemsIn(FileSystem.parentPath(each.path)))
-            console.debug(`each.path is:`,each.path)
-            console.debug(`output is:`,output)
-            console.debug(`error is:`,error)
-            continue
-        }
-        classes[Class] = eval(`(()=>{ class ${Class} {}; return ${Class} })()`)
-        const methods = {}
-        try {
-            console.debug(`{ File, Class, Author, Purpose, Functions } is:`,{ File, Class, Author, Purpose, Functions })
-            for (const eachFunctionNumber of Functions) {
-                console.group()
-                console.log(`loading ${eachFunctionNumber.toString(16)}`)
-                console.log(`        ${await run`git checkout ${eachFunctionNumber.toString(16)} ${Stdout(returnAsString)}`}`)
-                const jsFile = await FileSystem.read(`${parentPath}/${each.name}.js`)
-                const methodName = jsFile.match(new RegExp(`${Class}\\.prototype\\.(\\w+)`))[1]
-                
-                console.log(`aka ${methodName}`)
-                const tree = parser.parse({ string: jsFile, withWhitespace: true })
-                let newCode = ""
-                const allNodes = flatNodeList(tree.rootNode).map(each=>!each.hasChildren)
-                for (const [ nodeIndex, each ] of enumerate(allNodes)) {
-                    if (!each.hasChildren) {
-                        if (!(each.type == "comment")) {
-                            newCode += each.text||""
-                        } else {
-                            let text = each.text
-                            const remainingText = allNodes.slice(nodeIndex+1,).filter(each=>each.type!=="comment").map(each=>each.text).join("")
-                            // must try to make every bit of potentially-executable code executable
-
-                            // slice off the comment-y stuff
-                            if (text.startsWith("/*")) {
-                                text = text.slice(2,-2)
+    const classes = {}
+    FileSystem.cwd = Deno.args[0]
+    for (const each of filePaths) {
+        if (each.path.endsWith(".json")) {
+            console.group()
+            console.debug(`loading ${each.path}`)
+            const parentPath = FileSystem.parentPath(each.path)
+            console.log(`        ${await run`git checkout ${startingCommit} ${Stdout(returnAsString)}`}`)
+            const output = await FileSystem.read(each.path)
+            if (!output) {
+                console.debug(`each.path: ${each.path}`)
+            }
+            try {
+                var { File, Class, Author, Purpose, Functions } = JSON.parse(output)
+            } catch (error) {
+                console.debug(`await FileSystem.listFileItemsIn(FileSystem.parentPath(each.path)) is:`,await FileSystem.listFileItemsIn(FileSystem.parentPath(each.path)))
+                console.debug(`each.path is:`,each.path)
+                console.debug(`output is:`,output)
+                console.debug(`error is:`,error)
+                continue
+            }
+            classes[Class] = eval(`(()=>{ class ${Class} {}; return ${Class} })()`)
+            const methods = {}
+            try {
+                console.debug(`{ File, Class, Author, Purpose, Functions } is:`,{ File, Class, Author, Purpose, Functions })
+                for (const eachFunctionNumber of Functions) {
+                    console.group()
+                    console.log(`loading ${eachFunctionNumber.toString(16)}`)
+                    console.log(`        ${await run`git checkout ${eachFunctionNumber.toString(16)} ${Stdout(returnAsString)}`}`)
+                    const jsFile = await FileSystem.read(`${parentPath}/${each.name}.js`)
+                    const methodName = jsFile.match(new RegExp(`${Class}\\.prototype\\.(\\w+)`))[1]
+                    
+                    console.log(`aka ${methodName}`)
+                    const tree = parser.parse({ string: jsFile, withWhitespace: true })
+                    let newCode = ""
+                    const allNodes = flatNodeList(tree.rootNode).map(each=>!each.hasChildren)
+                    for (const [ nodeIndex, each ] of enumerate(allNodes)) {
+                        if (!each.hasChildren) {
+                            if (!(each.type == "comment")) {
+                                newCode += each.text||""
                             } else {
-                                text = text.slice(2)
-                            }
-                            
-                            const snippetIsValid = async (snippet)=>{
-                                try {
-                                    // if it passes eval, then its valid 😜
-                                    await eval(`${newCode}${snippet}${remainingText}`)
-                                    newCode += snippet
-                                    return true
-                                } catch (error) {
+                                let text = each.text
+                                const remainingText = allNodes.slice(nodeIndex+1,).filter(each=>each.type!=="comment").map(each=>each.text).join("")
+                                // must try to make every bit of potentially-executable code executable
+
+                                // slice off the comment-y stuff
+                                if (text.startsWith("/*")) {
+                                    text = text.slice(2,-2)
+                                } else {
+                                    text = text.slice(2)
+                                }
+                                
+                                const snippetIsValid = async (snippet)=>{
                                     try {
-                                        // gotta try automatic semicolon injection
-                                        await eval(`${newCode};${snippet}${remainingText}`)
-                                        newCode += ";"+snippet
+                                        // if it passes eval, then its valid 😜
+                                        await eval(`${newCode}${snippet}${remainingText}`)
+                                        newCode += snippet
                                         return true
                                     } catch (error) {
+                                        try {
+                                            // gotta try automatic semicolon injection
+                                            await eval(`${newCode};${snippet}${remainingText}`)
+                                            newCode += ";"+snippet
+                                            return true
+                                        } catch (error) {
+                                            return false
+                                        }
                                         return false
                                     }
                                     return false
                                 }
-                                return false
-                            }
-                            
-                            // gotta try all the combinations to make sure comments execute as valid code
-                            tryNext: while (true) {
-                                for (const [startIndex, _] of enumerate(text)) {
-                                    for (const [endIndex, __] of enumerate(text+" ").reverse()) {
-                                        if (await snippetIsValid(text.slice(startIndex, endIndex))) {
-                                            text = text.slice(endIndex)
-                                            // if there's still some text remaining, try to make it valid too
-                                            if (text.length != 0) {
-                                                continue tryNext
-                                            // otherwise were done
-                                            } else {
-                                                break tryNext
+                                
+                                // gotta try all the combinations to make sure comments execute as valid code
+                                tryNext: while (true) {
+                                    for (const [startIndex, _] of enumerate(text)) {
+                                        for (const [endIndex, __] of enumerate(text+" ").reverse()) {
+                                            if (await snippetIsValid(text.slice(startIndex, endIndex))) {
+                                                text = text.slice(endIndex)
+                                                // if there's still some text remaining, try to make it valid too
+                                                if (text.length != 0) {
+                                                    continue tryNext
+                                                // otherwise were done
+                                                } else {
+                                                    break tryNext
+                                                }
                                             }
                                         }
                                     }
+                                    break // ran out of characters
                                 }
-                                break // ran out of characters
+                            }
+                            try {
+                                classes[Class].prototype[methodName] = methods[methodName] = eval(newCode)
+                            } catch (error) {
+                                console.log(`sending an email to ${Author}: ${JSON.stringify(methodName)} didnt work: ${error}`)
+                                console.debug(`error.stack is:`,error.stack)
                             }
                         }
-                        try {
-                            classes[Class].prototype[methodName] = methods[methodName] = eval(newCode)
-                        } catch (error) {
-                            console.log(`sending an email to ${Author}: ${JSON.stringify(methodName)} didnt work: ${error}`)
-                            console.debug(`error.stack is:`,error.stack)
-                        }
+                    }
+                    console.groupEnd()
+                }
+                // call constructor if it exists
+                if (Object.keys(methods).includes("constructor")) {
+                    try {
+                        const newObject = new classes[Class]()
+                        // call the constructor
+                        console.debug(`methods is:`,methods)
+                        await methods.constructor.apply(newObject, {})
+                    } catch (error) {
+                        console.log(`sending an email to ${Author}: ${JSON.stringify("constructor")} didnt work: ${error}`)
+                        console.debug(`error.stack is:`,error.stack)
                     }
                 }
-                console.groupEnd()
+            } catch (error) {
+                console.log(`sending an email to ${Author}: ${JSON.stringify(error)}, ${error}`)
+                console.debug(`error.stack is:`,error.stack)
             }
-            // call constructor if it exists
-            if (Object.keys(methods).includes("constructor")) {
-                try {
-                    const newObject = new classes[Class]()
-                    // call the constructor
-                    console.debug(`methods is:`,methods)
-                    await methods.constructor.apply(newObject, {})
-                } catch (error) {
-                    console.log(`sending an email to ${Author}: ${JSON.stringify("constructor")} didnt work: ${error}`)
-                    console.debug(`error.stack is:`,error.stack)
-                }
-            }
-        } catch (error) {
-            console.log(`sending an email to ${Author}: ${JSON.stringify(error)}, ${error}`)
-            console.debug(`error.stack is:`,error.stack)
+            console.groupEnd()
         }
-        console.groupEnd()
+        
     }
-    
-}
 
-await run`git checkout ${startingCommit}`
+    await run`git checkout ${startingCommit}`
+} catch (error) {
+    await run`git checkout master`
+}
