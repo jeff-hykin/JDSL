4f71495bb906d633a2cc11a58f264bf2f2cbfbc2
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-23 21:01:55 -0500

    '--'

--- JDSL.ps1
+++ JDSL.ps1
@@ -12,7 +12,7 @@ import javascript from "https://github.com/jeff-hykin/common_tree_sitter_languag
 
 const parser = await parserFromWasm(javascript)
 
-const debug = false
+const debug = true
 
 try {
     const filePaths = await FileSystem.listFileItemsIn(Deno.args[0])
