1327e8a7a6caae3822b54a1b6f2f9e696ed1daed
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-23 21:21:26 -0500

    -

--- the_system/index.js
+++ the_system/index.js
@@ -1,7 +1,8 @@
 Index.prototype.constructor = function()
 {
     console.log("Howdy")
-
+    
+    console.debug(`this is:`,this)
     // console.log("yup this executes")
     /*  this.DropTables() */
 }
