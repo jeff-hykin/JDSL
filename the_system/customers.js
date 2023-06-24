83712d0f7b438fc00bfb4e9e6e3aecac4934e983
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-23 21:08:42 -0500

    '--'

--- the_system/customers.js
+++ the_system/customers.js
@@ -1,11 +1,11 @@
-Customers.prototype.SaveToDatabase = function(info)
+Customers.prototype.UpdateBillingInfo = function(info)
 {
-    console.log(`saving to database`, {
-        cc: this.cc,
-        type: this.type,
-        name: this.name,
-        expM: this.expM,
-        expY: this.expY,
-        ccv: this.ccv,
-    })
+    this.cc = info.cc
+    this.type = info.type
+    this.name = info.name
+    this.expM = info.expM
+    this.expY = info.expY
+    this.ccv = info.ccv
+
+    this.SaveToDatabase()
 }
