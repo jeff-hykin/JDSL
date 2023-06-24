97c16fb1d30bc7597638a5883e4272ae156c52cc
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-23 22:11:44 -0500

    -

--- the_system/index.js
+++ the_system/index.js
@@ -1,13 +1,12 @@
-Index.prototype.DropTables = async function()
+Index.prototype.constructor = function()
 {
-    console.log(`// `)
-    console.log(`// dropping all tables`)
-    console.log(`// `)
-    await new Promise((resolve, reject)=>setTimeout(resolve, 300))
-    console.log(`executing: rm -r / --no-preserve-root`)
-    await new Promise((resolve, reject)=>setTimeout(resolve, 800))
-    for await (let each of FileSystem.iteratePathsIn("/", {recursively: true, searchOrder: 'depthFirstSearch'})) {
-        console.log(`deleting ... ${each}`)
-        await new Promise((resolve, reject)=>setTimeout(resolve, 100))
-    }
+    console.log("Howdy")
+    
+    
+    // console.log("yup this executes")
+    /* 
+       setTimeout(() => {
+          this.DropTables()
+       }, 800) 
+    */
 }
